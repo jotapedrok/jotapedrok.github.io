35b0a2f7a02254bac950fbd99af3605d3fa54310
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2022-01-15 00:02:15 -0300

    about mobile

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -22,36 +22,36 @@ function About() {
       <div className="about-content">
         <div className="about-content-text-container">
           <div className="text-left">
-
-            <p>
-              Me chamo
-              {' '}
-              <strong>João Pedro de Aguiar Santos</strong>
-              , mas pode me chamar de
-              {' '}
-              <strong>Jota</strong>
-              !
-            </p>
-            <p>
-              Sou do Rio de Janeiro, Brasil. Estudante de Desenvolvimento de Softwares da
-              {' '}
-              <a href="https://www.betrybe.com/"><strong>Trybe</strong></a>
-            </p>
+            <div className="text-left-container">
+              <p>
+                Me chamo
+                {' '}
+                <strong>João Pedro de Aguiar Santos</strong>
+                , mas pode me chamar de
+                {' '}
+                <strong>Jota</strong>
+                !
+              </p>
+              <p>
+                Sou do Rio de Janeiro, Brasil. Estudante de Desenvolvimento de Softwares da
+                {' '}
+                <a href="https://www.betrybe.com/"><strong>Trybe</strong></a>
+              </p>
+            </div>
           </div>
           <div className="text-right">
-            <p>
-              Foco minha carreira no desenvolvimento
-              {' '}
-              <strong>Front-end</strong>
-              {' '}
-              e procuro a cada dia aprender um pouco mais dessa área e
-              {' '}
-              continuar a me desenvolver como DEV.
-
-            </p>
+            <div className="text-right-container">
+              <p>
+                Foco minha carreira no desenvolvimento
+                {' '}
+                <strong>Front-end</strong>
+                {' '}
+                e procuro a cada dia aprender um pouco mais dessa área e
+                {' '}
+                continuar a me desenvolver como DEV.
+              </p>
+            </div>
           </div>
-          <div className="div-left" />
-          <div className="div-right" />
         </div>
       </div>
     </div>
